be2eece36c1e2020c68c4d96e2a84d5eea9b9327
Jacob Hutchinson <jhutchinson.programmer@gmail.com>
2022-02-18 14:45:08 -0500

    Added proper layout to GUI

--- ToolGUI.py
+++ ToolGUI.py
@@ -1,6 +1,8 @@
 import tkinter as tk
-from tkinter import Button, Canvas, Frame
+from tkinter import Button, Canvas, Frame, OptionMenu, StringVar
 from tkinter.constants import BOTH, RIGHT, X
+import itertools
+from gevent import config
 
 from ToolLogic import FarmLogic
 
@@ -36,15 +38,25 @@ class ToolClient():
         encrypt_button = Button(window, text='Encrypt',bg='#343130', activebackground='#292726',relief='flat', font='bold', fg='#c4c4c4', activeforeground='#4764f5', command=encrypt)
         decrypt_button = Button(window, text='Decrypt',bg='#343130', activebackground='#292726',relief='flat', font='bold', fg='#c4c4c4', activeforeground='#4764f5', command=decrypt)
 
+
+        config_text = StringVar()
+        config_text.set('Default')
+        options = FarmLogic.load_config()
+        select_config = OptionMenu(window, config_text, *dict(itertools.islice(options.items(),4)))
+        select_config.configure(bg='#343130', highlightthickness=0, relief='flat', font='bold', fg='#c4c4c4', activebackground='#383534', activeforeground='#4764f5')
+        select_config["menu"].configure(bg='#524d4b', fg='#e8e8e8')
+        #use ["menu"] to edit the drop down section
+        print(select_config.keys())
+
         root_window.geometry('500x250+600+200')
         root_window.title('Encryption')
         root_window['background'] = '#343130'
 
-        select_button.place(x=75,y=90)
+        select_config.place(x=75,y=90)
         encrypt_button.place(x=375,y=90)
         decrypt_button.place(x=225,y=90)
 
-        select_button.pack
+        select_config.pack
         encrypt_button.pack
         decrypt_button.pack
 
